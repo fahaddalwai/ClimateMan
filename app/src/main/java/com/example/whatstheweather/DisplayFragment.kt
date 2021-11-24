2fc247227876c9fb988a3595e9611044caa86c81
dalwa <dalwaifahad@gmail.com>
2021-11-24 22:57:57 +0300

    list adapter added

--- app/src/main/java/com/example/whatstheweather/DisplayFragment.kt
+++ app/src/main/java/com/example/whatstheweather/DisplayFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import com.example.favfoodroom.viewfavfood.WeatherAdapter
 import com.example.whatstheweather.databinding.FragmentDisplayBinding
 import com.example.whatstheweather.feature.presentation.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,9 +22,22 @@ class DisplayFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding= DataBindingUtil.inflate(inflater,R.layout.fragment_display, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_display, container, false)
         viewModel
+
+
+        binding.viewModel=viewModel
+        binding.lifecycleOwner=this
+
+        val adapter=WeatherAdapter()
+        binding.recyclerView.adapter= adapter
+
+        viewModel.state.observe(viewLifecycleOwner,{
+            it?.let{
+                adapter.submitList(it.weatherInfoItems)
+            }
+        })
 
         return binding.root
     }
